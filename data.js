13abe3dc6213b954a8298074bc1fdff30f9cc403
fujinxiang <fujinxiang@163.com>
2024-01-20 12:11:34 +0800

    更新data

--- data.js
+++ data.js
@@ -1,10 +1,14 @@
 window.tools = [
     {
         name: 'Fabric 背景生成',
-        path: './fabric'
+        path: './fabric/'
+    },
+    {
+        name: 'Chat',
+        path: 'https://i.fujinxiang.cn/'
     },
     {
         name: 'WebTorrent',
-        path: './webtorrent'
+        path: './webtorrent/'
     },
 ]
--- docs.md
+++
@@ -1,5 +0,0 @@
-
-git push gitee main 推送到 gitee
-
-<https://gitee.com/fujinxiang/fujinxiang/pages>
-
